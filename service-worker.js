9114c014113a395bc7dcb57deb3cb595b736abf0
jendrik98 <jendrik98@gmx.net>
2026-09-06 14:58:17 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="mietverwaltung-v17-0-0";
-const CORE=["./","./index.html","./style.css","./app.js?v=1602","./v17-upgrade.js?v=1700","./manifest.webmanifest","./legal-rules.json","./icon-192.png","./icon-512.png"];
+const CACHE="mietverwaltung-v17-0-1";
+const CORE=["./","./index.html","./style.css","./app.js?v=1602","./v17-upgrade.js?v=1700","./v17-hotfix.js?v=1701","./manifest.webmanifest","./legal-rules.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("message",e=>{if(e.data?.type==="SKIP_WAITING")self.skipWaiting()});
